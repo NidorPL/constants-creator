7c43dbb471b975380b134dbf747cb90729a49949
David Schmidt <david.schmidt@campus.tu-berlin.de>
2018-10-25 18:46:51 +0200

    :memo: update readme

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import React, { Component } from 'react';
+import { Input } from "reactstrap"
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    value : `const G8_LANGUAGE_MAP = {
+              [ENGLISH]: 2057,
+              [GERMAN]: 1031,
+              [ARABIC]: 10241,
+          }`
+  }
+
+
   render() {
-    return (
+      return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
+          <Input className="MainInputArea" type="textarea" name="text" id="exampleText"
+          value= {this.state.value}
+          />
       </div>
     );
   }
