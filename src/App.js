9cf849dea1f8475b27871766510294ade2c50d1c
David Schmidt <david.schmidt@campus.tu-berlin.de>
2018-10-27 01:31:45 +0200

    :sparkles: calculation of variables completed

--- src/App.js
+++ src/App.js
@@ -6,12 +6,70 @@ import './App.css';
 class App extends Component {
   state = {
     value : `const G8_LANGUAGE_MAP = {
-              [ENGLISH]: 2057,
-              [GERMAN]: 1031,
-              [ARABIC]: 10241,
-          }`
+                [ENGLISH]: 2057,
+                [GERMAN]: 1031,
+                [ARABIC]: 10241,
+            }`
+  }
+  handleChange = this.handleChange.bind(this)
+  renderConsts = this.parseConst.bind(this)
+
+  componentDidMount() {
+      this.parseConst(this.state.value)
   }
 
+  handleChange(i) {
+      this.setState({value: i.target.value})
+      try {
+      this.parseConst(i.target.value)
+    }catch (e) {
+
+    }
+  }
+
+  getKey(c) {
+      let beginning = c.indexOf("[")
+      let ending = c.indexOf("]")
+      return c.substring(beginning+1, ending)
+  }
+  getValue(c) {
+      let beginning = c.indexOf(":")
+      return c.substring(beginning+1)
+  }
+  getJSONFromString(string, array) {
+      string = string.replace(/\n/g,'')
+      string = string.replace(/ /g,'')
+      let constantsArray = string.substring(22,66).split(",")
+      constantsArray.pop()
+
+
+
+
+    let constantsJson = {}
+      constantsArray.forEach(c => {
+          let key = this.getKey(c)
+          let value = this.getValue(c)
+          constantsJson[key] = value
+      })
+      return constantsJson
+
+  }
+  parseConst(c) {
+      const mapTitle = c.substring(6,21)
+      console.log("trying to iterate");
+      let json = this.getJSONFromString(c)
+
+      let constString = ""
+      let constsObject = "const consts = { \n"
+      constsObject += mapTitle + ", \n"
+
+      for(let k in json) {
+          constString += "const " + k + " = " + "\"" + k + "\"" + "\n"
+          constsObject += k + ",\n"
+      }
+      constsObject += "}"
+     const moduleExports = "module.exports = consts"
+  }
 
   render() {
       return (
@@ -20,7 +78,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
         </header>
           <Input className="MainInputArea" type="textarea" name="text" id="exampleText"
-          value= {this.state.value}
+          value= {this.state.value} onChange={this.handleChange}
           />
       </div>
     );
